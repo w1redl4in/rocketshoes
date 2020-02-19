d5a9e1229cfd2d31c4b6db147003b15c307ef49d
Felipe <felipe.araujo@hubfintech.com.br>
2020-02-19 17:34:50 -0300

    bussiness rule moved to redux sagas

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,15 +4,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const product = draft.findIndex(p => p.id === action.product.id);
-        if (product >= 0) {
-          draft[product].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        const { product } = action;
+
+        draft.push(product);
       });
 
     case '@@cart/REMOVE_FROM_CART':
