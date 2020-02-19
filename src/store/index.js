5e3aa27082c7ce29623ac9706fa073cb6cc0ce63
Felipe <felipe.araujo@hubfintech.com.br>
2020-02-19 17:25:23 -0300

    lib for reactotron capture every side effect

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,13 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './rootReducer';
 import rootSaga from './rootSaga';
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+const sagaMiddleware = createSagaMiddleware({
+  sagaMonitor,
+});
 
 const enhancer =
   process.env.NODE_ENV === 'development'
